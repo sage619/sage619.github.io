6e6c593c3aaf53b1bf7596ff2316db3a9048086c
Sage <118636222+sage619@users.noreply.github.com>
2023-04-19 14:51:47 -0500

    Update button.java

--- button.java
+++ button.java
@@ -1,4 +1,4 @@
-<img src="pic.jpg" id="myPicture" />
+<img src="https://i.pinimg.com/564x/af/44/41/af44415a12b12435f959b7183af4b980.jpg" id="WOLF" />
 <script type="text/javascript">
     $(document).ready(function(){
         $('#myPicture').on('click', function(){
